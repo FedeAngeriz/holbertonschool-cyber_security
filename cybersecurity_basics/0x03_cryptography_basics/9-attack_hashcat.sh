b9c0455d997bcdbeff08c1428597bd1d815834d2
FedeAngeriz <10086@holbertonstudents.com>
2025-10-23 00:02:38 -0300

    file 9

---
+++ cybersecurity_basics/0x03_cryptography_basics/9-attack_hashcat.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+hashcat --stdout wordlist1.txt wordlist2.txt | hashcat -m 0 -a 0 "$1" --quiet 2>/dev/null || true && printf '<%s>\n' "$(hashcat --show -m 0 "$1" 2>/dev/null | awk -F: 'NF>=2{print $NF}' | paste -sd, - )" > 9-password.txt
