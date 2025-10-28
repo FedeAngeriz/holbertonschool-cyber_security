cfa7f39a9d04ee18e6e97c6e8bfdffac33522a97
FedeAngeriz <10086@holbertonstudents.com>
2025-10-28 12:20:48 -0300

    file 6

--- linux_security/0x03_protocols_servers/6-smtp.sh
+++ linux_security/0x03_protocols_servers/6-smtp.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -q "STARTTLS" /etc/postfix/main.cf || echo "STARTTLS not configured"
+grep ^smtpd_tls_security_level -q "STARTTLS" /etc/postfix/main.cf || echo "STARTTLS not configured"
