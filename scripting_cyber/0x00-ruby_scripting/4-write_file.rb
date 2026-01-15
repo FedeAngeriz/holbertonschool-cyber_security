e491f11437b780751cf2ccac382c4783a48501fc
FedeAngeriz <10086@holbertonstudents.com>
2026-01-15 01:34:54 -0300

    file 4

--- scripting_cyber/0x00-ruby_scripting/4-write_file.rb
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -2,13 +2,10 @@
 require 'json'
 
 def merge_json_files(file1_path, file2_path)
-    file1_content = File.read(file1_path)
-    file2_content = File.read(file2_path)
+    json1 = JSON.parse(File.read(file1_path))
+    json2 = JSON.parse(File.read(file2_path))
+    
+    mergex = json2 + json1
 
-    json1 = JSON.parse(file1_content)
-    json2 = JSON.parse(file2_content)
-
-    mergex_json = json2.merge(json1)
-
-    File.write(file2_path, JSON.pretty_generate(mergex_json))
+    File.write(file2_path, JSON.pretty_generate(mergex))
 end
