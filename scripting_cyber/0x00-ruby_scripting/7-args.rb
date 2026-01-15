87a0a1f583451ea3903acb7f0deaf17c08c4ebad
FedeAngeriz <10086@holbertonstudents.com>
2026-01-15 01:45:50 -0300

    file 7

--- scripting_cyber/0x00-ruby_scripting/7-args.rb
+++ scripting_cyber/0x00-ruby_scripting/7-args.rb
@@ -1,10 +1,10 @@
 #!/usr/bin/env ruby
-def print_arguments
-    if ARGV.empty?
-        puts "No arguments"
-    else
-        puts "Arguments:#{ARGV.join}"
-    end
-end
 
-print_arguments
+def print_arguments
+  puts "Arguments:"
+  puts
+
+  ARGV.each_with_index do |arg, index|
+    puts "#{index + 1}. #{arg} "
+  end
+end
