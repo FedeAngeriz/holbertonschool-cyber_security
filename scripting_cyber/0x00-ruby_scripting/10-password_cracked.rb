48d8ca9cb80bf7f72b6675c4b7e8e738337c87e7
FedeAngeriz <10086@holbertonstudents.com>
2026-01-15 00:36:42 -0300

    file 10

--- scripting_cyber/0x00-ruby_scripting/10-password_cracked.rb
+++ scripting_cyber/0x00-ruby_scripting/10-password_cracked.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'digest'
-/
+
 def dictionary_attack(hashed_password, dictionary_file)
     File.foreach(dictionary_file) do |word|
         word = word.chomp
@@ -25,27 +25,3 @@ hashed_password = ARGV[0]
 dictionary_file = ARGV[1]
 
 dictionary_attack(hashed_password, dictionary_file)
-/
-def crack_password
-  if ARGV.length != 2
-    puts "Usage: 10-password_cracked.rb HASHED_PASSWORD DICTIONARY_FILE"
-    return
-  end
-
-  hashed_password = ARGV[0]
-  dictionary_file = ARGV[1]
-
-  File.foreach(dictionary_file) do |line|
-    word = line.strip
-    hashed_word = Digest::SHA256.hexdigest(word)
-
-    if hashed_word == hashed_password
-      puts "Password found: #{word}"
-      return
-    end
-  end
-
-  puts "Password not found in dictionary."
-end
-
-crack_password
